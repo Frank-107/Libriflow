69aa8504240715fcd97b799f2e1ee7dd22b7f4b2
frank-107 <20253ds107@utez.edu.mx>
2026-07-03 19:27:23 -0600

    guardar contrasenas en credencial

--- src/main/java/mx/edu/utez/libriflow/model/Dao/UsuarioDao.java
+++ src/main/java/mx/edu/utez/libriflow/model/Dao/UsuarioDao.java
@@ -3,17 +3,17 @@ package mx.edu.utez.libriflow.model.Dao;
 import mx.edu.utez.libriflow.model.Usuario;
 import mx.edu.utez.libriflow.utils.SQLconnector;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 
-public class UsuarioDao implements Dao<Usuario, Integer> {
-    @Override
-    public boolean create(Usuario entidad) {
+public class UsuarioDao {
+    public int create(Usuario entidad) {
         String sql = "INSERT INTO Usuario(nombre, apellido_paterno, apellido_materno, correo_electronico, telefono, estado_cuenta) VALUES(?, ?, ?, ?, ?, ?)";
         try (Connection con = SQLconnector.getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
+             PreparedStatement ps = con.prepareStatement(
+                     sql,
+                     new String[]{"ID_USUARIO"}
+             );) {
             ps.setString(1, entidad.getNombre());
             ps.setString(2, entidad.getApellidoPaterno());
             ps.setString(3, entidad.getApellidoMaterno());
@@ -22,30 +22,32 @@ public class UsuarioDao implements Dao<Usuario, Integer> {
             ps.setString(6, "ACTIVA"); // Estado de la cuenta por defecto
 
             int filasAfectadas = ps.executeUpdate();
-            return filasAfectadas > 0;
+            if (filasAfectadas == 0) throw new SQLException("No se pudo insertar el usuario.");
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()) {
+                return rs.getInt(1);
+            }
+            throw new SQLException("No se pudo obtener el ID del usuario insertado.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
-            return false;
+            return -1;
         }
     }
 
-    @Override
+
     public java.util.List<Usuario> getAll() {
         return null;
     }
 
-    @Override
     public Usuario getById(Integer id) {
         return null;
     }
 
-    @Override
     public boolean update(Usuario entidad) {
         return false;
     }
 
-    @Override
     public boolean delete(Integer id) {
         return false;
     }
